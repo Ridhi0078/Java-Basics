9d61d94105d77506a886e526e789077ec2999250
Ridhima Dangwal <145930316+Ridhi0078@users.noreply.github.com>
2025-08-24 22:12:17 +0530

    Delete Exception1.java

--- Exception1.java
+++
@@ -1,9 +0,0 @@
-public class Exception1 {
-    public static void main(String[] args) {
-        try{
-            int a=10/0;
-        } catch(ArithmeticException e){
-            System.out.println(e.getMessage());
-        }
-    }
-}
