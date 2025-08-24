68256ad9a127a7d245870cefc0adbd8b872d9d7b
Ridhima Dangwal <145930316+Ridhi0078@users.noreply.github.com>
2025-08-24 22:31:42 +0530

    Update and rename Trapped.java to TrappedWater.java

--- TrappedWater.java
+++ TrappedWater.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Trapped {
+public class TrappedWater {
     public static int Trapped_water(int height[]){
          // calculate left max boundary : array
          int n=height.length;
